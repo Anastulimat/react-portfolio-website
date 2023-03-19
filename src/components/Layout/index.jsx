da487b7a02e30c92c5e8d23382abca601a0268ce
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-03-19 21:30:28 +0100

    feat: add some styling to sidebar logo

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,12 @@
 import './index.scss';
 import React from 'react';
+import Sidebar from '../Sidebar';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+        <Sidebar />
+    </>
   )
 }
 
